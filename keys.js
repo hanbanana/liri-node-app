e83eb46a443c52529a80a00646cd0f2de054a5fc
Han <2han@live.com>
2018-06-23 18:51:29 -0700

    added files

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'xuUTCr2F3sPEhBALSou6QRSgf',
+    consumer_secret: 'cgb1xA25cP0ZnKTRaLuVXhpxncOgIXUq6BqSwuUsCpfFS6r4oJ',
+    access_token_key: '1009993748774699008-JQah3r8WCTvHAFSlwx23q21Cs82CiH',
+    access_token_secret: 'k7YKEHIAZ8QhMQpVUAcRhIY2RTMcQDTJVlFYjKSl1AVwM'
+  }
+
+exports.spotify = {
+  id: '03c71c06584b4b63bead3aa17eaefc51',
+  secret: '1e5b929509c94865a9fa80d225f9fdb2'
+};
